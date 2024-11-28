416710382dfcf68d5170933d6d18750f5d38dcc1
Cong-Thanh Vu <vuthanh.cdt@gmail.com>
2024-11-28 13:49:36 +0800

    update

--- velodyne_gazebo_plugins/include/velodyne_gazebo_plugins/GazeboRosVelodyneLaser.hpp
+++ velodyne_gazebo_plugins/include/velodyne_gazebo_plugins/GazeboRosVelodyneLaser.hpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2015-2018, Dataspeed Inc.
+ *  Copyright (c) 2015-2021, Dataspeed Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -35,12 +35,7 @@
 #ifndef GAZEBO_ROS_VELODYNE_LASER_H_
 #define GAZEBO_ROS_VELODYNE_LASER_H_
 
-#include <string>
-#include <vector>
-
 #include <sdf/Param.hh>
-
-#include <ignition/math/Angle.hh>
 
 #include <gazebo/transport/Node.hh>
 
@@ -57,22 +52,6 @@ namespace gazebo
 
   class GazeboRosVelodyneLaser : public SensorPlugin
   {
-    struct ScanPattern {
-      struct Sample {
-        Sample() = default;
-
-        Sample(double _index, ignition::math::Angle _angle)
-          : index(_index), angle(_angle)
-        {
-        }
-
-        double index{0.};
-        ignition::math::Angle angle{0.0};
-      };
-      std::vector<Sample> horizontal_samples;
-      std::vector<Sample> vertical_samples;
-    };
-
     /// \brief Constructor
     /// \param parent The parent entity, must be a Model or a Sensor
     public: GazeboRosVelodyneLaser();
@@ -84,49 +63,28 @@ namespace gazebo
     /// \param take in SDF root element
     public: void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf);
 
-    /// \brief Load scan pattern
-    /// \param _parent Parent (gpu) ray sensor
-    /// \param _sdf Root sensor plugin SDF element
-    private: void LoadScanPattern(sensors::SensorPtr _parent, sdf::ElementPtr _sdf);
-
-    /// \brief Sample angle interval evenly
-    /// \param _min_angle Angle interval lower bound
-    /// \param _max_angle Angle interval upper bound
-    /// \param _sample_count Amount of samples to take
-    private: std::vector<ScanPattern::Sample> SampleAngleIntervalEvenly(ignition::math::Angle _min_angle,
-                                                                        ignition::math::Angle _max_angle,
-                                                                        int _sample_count);
-
-    /// \brief Sample angle interval as configured.
-    /// \param _sdf Root SDF element holding configuration
-    /// \param _min_angle Angle interval lower bound
-    /// \param _max_angle Angle interval upper bound
-    /// \param _sample_count Amount of samples to take
-    private: std::vector<ScanPattern::Sample> SampleAngleInterval(sdf::ElementPtr _sdf, ignition::math::Angle _min_angle,
-                                                                  ignition::math::Angle _max_angle, int _sample_count);
-
     /// \brief Subscribe on-demand
     private: void ConnectCb();
 
     /// \brief The parent ray sensor
-    private: sensors::SensorPtr parent_sensor_;
+    private: sensors::SensorPtr parent_ray_sensor_;
 
     private: gazebo_ros::Node::SharedPtr ros_node_;
 
     /// \brief ROS publisher
     private: rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_;
 
-    /// \brief topic name
-    private: std::string topic_name_;
+    /// \brief ROS timer to emulate publisher connection callback
+    private: rclcpp::TimerBase::SharedPtr timer_;
 
     /// \brief frame transform name, should match link name
     private: std::string frame_name_;
 
+    /// \brief organize cloud
+    private: bool organize_cloud_;
+
     /// \brief the intensity beneath which points will be filtered
     private: double min_intensity_;
-
-    /// \brief the expected ray casting pattern for this laser.
-    private: ScanPattern scan_pattern_;
 
     /// \brief Minimum range to publish
     private: double min_range_;
@@ -147,14 +105,25 @@ namespace gazebo
       return sigma * (sqrt(-2.0 * ::log(U)) * cos(2.0 * M_PI * V)) + mu;
     }
 
-    /// \brief For setting ROS name space
-    private: std::string robot_namespace_;
+    /// \brief A mutex to lock access
+    private: std::mutex lock_;
 
     // Subscribe to gazebo laserscan
     private: gazebo::transport::NodePtr gazebo_node_;
     private: gazebo::transport::SubscriberPtr sub_;
     private: void OnScan(const ConstLaserScanStampedPtr &_msg);
 
+    /// \brief Re-implementation of old tf::resolve
+    private: static std::string tf_resolve(const std::string& prefix, const std::string& frame_id)
+    {
+      std::string output;
+      if (prefix.empty()) {
+        output = frame_id;
+      } else {
+        output = prefix + "/" + frame_id;
+      }
+      return output;
+    }
   };
 
 } // namespace gazebo
